81947d959cf991ae351f058af5f8cd824035867b
danrolo <darobles@ciencias.unam.mx>
2023-11-17 13:30:41 -0600

    feat (app): Implamenting requests rate limit

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -7,11 +8,22 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// -------------- 1. Middleware (start)
+// -------------- 1. Global Middleware (start)
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // 3rd party middleware that notifies info about the requests
 }
+
+// 3rd party middleware that limits the number of requests from an IP
+// (DDOS protection)
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour'
+});
+
+// This doesn't work when the application crashes and restarts
+app.use('/api', limiter); // only apply to /api (the first argument is optional)
 
 // Middleware: a function that can modify the incoming request/response objects in Express
 // (in the request/response cycle)
@@ -20,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public/`));
-// -------------- 1. Middleware (end)
+// -------------- 1. Global Middleware (end)
 
 // -------------- 2. Mount Routes (start)
 app.use('/api/v1/tours', tourRouter);
