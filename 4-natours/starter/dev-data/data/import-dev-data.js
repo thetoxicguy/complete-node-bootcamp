7f9a99f5736dfeb09a7834024b344b4a43ec69fc
danrolo <darobles@ciencias.unam.mx>
2023-11-29 07:56:05 -0600

    fix (import-dev-data): Modified to import reviews and users (also User model temporarily)

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -3,8 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
-const filePath = path.join(__dirname, '/tours.json');
+const filePath1 = path.join(__dirname, '/tours.json');
+const filePath2 = path.join(__dirname, '/users.json');
+const filePath3 = path.join(__dirname, '/reviews.json');
 
 dotenv.config({ path: './config.env' });
 
@@ -29,14 +33,21 @@ db.once('open', function() {
 });
 
 // ---------- Reading the file
-const tours = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+const tours = JSON.parse(fs.readFileSync(filePath1, 'utf8'));
+const users = JSON.parse(fs.readFileSync(filePath2, 'utf8'));
+const reviews = JSON.parse(fs.readFileSync(filePath3, 'utf8'));
 // eslint-disable-next-line no-console
-console.log('File path:', filePath);
+console.log('File path:', filePath1);
+console.log('File path:', filePath2);
+console.log('File path:', filePath3);
+
 // ---------- Import data into database
 // JSON.parse is used because mongoose uses JS objects, not JSON
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false }); // Disabled validation for PasswordConfirm
+    await Review.create(reviews);
     // eslint-disable-next-line no-console
     console.log('Data successfully loaded');
   } catch (err) {
@@ -51,6 +62,8 @@ const deleteData = async () => {
   try {
     // Deletes all documents in collection because we do not specify
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     // eslint-disable-next-line no-console
     console.log('Data successfully deleted');
   } catch (err) {
