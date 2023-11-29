ce72f909b2509c004128e07e86e1e67ba71a7b49
danrolo <darobles@ciencias.unam.mx>
2023-11-29 10:21:01 -0600

    feat (reviewModel): Aggregation for avg ratings on new review

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -50,6 +51,43 @@ reviewSchema.pre(/^find/, function(next) {
     select: 'name photo'
   });
   next();
+});
+
+/*
+This static method sets an aggregation for the model itself
+*/
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  // 'this' points to the model: Review
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId } // Select all reviews that match the tourId
+    },
+    {
+      $group: {
+        _id: '$tour', // Group by tour
+        nRating: { $sum: 1 }, // Add 1 for each review that matches the tourId
+        avgRating: { $avg: '$rating' } // Calculate the average rating for this aggregation
+      }
+    }
+  ]);
+  // console.log(stats);
+  // Update the tour with the new stats, we don't need to save it
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+/*
+This middleware is to add
+the average rating and the number of ratings to the tour document
+*/
+reviewSchema.post('save', function() {
+  /*
+  'this' points to current review (this.constructor - Review model)
+  We call the static method on the model
+  */
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
